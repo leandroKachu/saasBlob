3766da8eff7540ed2a620ff6809595e924062951
leandroKachu <lesilvajunior@hotmail.com>
2022-11-17 19:49:19 -0800

    finish study

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
+  get "pricing", to: "static_pages#pricing"
   root "posts#index"
   resources :posts
+  post "checkout/create", to: "checkout#create", as: "checkout_create"
+  post "billing_portal/create", to: "billing_portal#create", as: "billing_portal_create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
